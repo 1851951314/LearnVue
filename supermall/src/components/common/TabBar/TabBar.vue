e5945a6c44b0cdd4c482dc3a9e13fd049fa16787
1851951314 <1851951314@qq.com>
2021-02-05 16:53:00 +0800

    购物车页面完成，为封装吐司模块做初步铺垫

---
+++ supermall/src/components/common/TabBar/TabBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#tab-bar {
+  background-color: #f6f6f6;
+  height: 49px;
+  border-top: 1px solid #eee;
+  box-shadow: 0px -1px 1px rgba(150, 150, 150, 0.8);
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  display: flex;
+  text-align: center;
+}
+</style>
